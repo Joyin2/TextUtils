9b7bdca0018fb56eb6cc91a6e5fc2f8e930418f5
Joyin2 <joyinlaskar2000@gmail.com>
2022-09-22 16:09:45 +0530

    login added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
-import Navbar from "./components/Navbar";
+// import Navbar from "./components/Navbar";
 import SignUp from "./components/SignUp";
 
 function App() {
   return (
     <>
-      <Navbar title= 'Assignment' />
+      {/* <Navbar title= 'Assignment' /> */}
       <SignUp/>
       
     </>
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,17 +1,47 @@
-import React from 'react';
-import signupImage from '../images/image-illustrator.jpg'
+import React from "react";
+import signupImage from "../images/image-illustrator.jpg";
 import "./SIgnUp.css";
 
 export default function SignUp() {
   return (
     <div>
-      <div className="container">
+      <div className="ls-container">
         <div className="image-container">
           <img src={signupImage} alt="" />
         </div>
         <div className="signup-container">
           <div className="signup-item">
-            
+            <h1>Sign Up</h1>
+            <h7>Enter your username, email and password to sign up</h7>
+            <form action="" method="post">
+              <input
+                className="form-control"
+                type="text"
+                placeholder="First Name"
+              ></input>
+              <input
+                className="form-control"
+                type="text"
+                placeholder="Last Name"
+              ></input>
+              <input
+                className="form-control"
+                type="email"
+                placeholder="Email"
+              ></input>
+              <input
+                className="form-control"
+                type="password"
+                placeholder="First Name"
+              ></input>
+
+              <button type="submit" className="button ">
+                Submit
+              </button>
+            </form>
+            <div className="already-ask">
+              Already have an account?&nbsp;<a href="/">Log in</a>
+            </div>
           </div>
         </div>
       </div>
