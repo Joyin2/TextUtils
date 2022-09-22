ff420f37f4e2242b255aad9820e1e3fa464d43c6
Joyin2 <joyinlaskar2000@gmail.com>
2022-09-22 13:03:37 +0530

    adding features

---
+++ src/components/SignUp.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import signupImage from '../images/image-illustrator.jpg'
+import "./SIgnUp.css";
+
+export default function SignUp() {
+  return (
+    <div>
+      <div className="image-container">
+        <img src={signupImage} alt="" />
+
+        
+      </div>
+      <div className="signup-container">
+        
+      </div>
+    </div>
+  );
+}
